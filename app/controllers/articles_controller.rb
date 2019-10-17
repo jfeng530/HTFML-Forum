3ca75133de5a549c9c602eaaa559240adecd65c8
Emirhan Kaplan <emskaplann@gmail.com>
2019-10-17 10:08:21 -0400

    asfa

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,36 +1,38 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update, :destroy]
-  
+
+
+    layout "show_lo", :only => [:show]
     # GET /articles
     # GET /articles.json
     def index
       @category = Category.find(params[:category_id])
-  
+
       if params[:search]
         #MAKE SEARCH WITH SQL COMMAND ## W H E R E ##
         @articles = Article.all.select{|art| art.title.downcase.include?(params[:search].downcase)}
         if @articles == []
           @s = true
         else
-          
+
         end
       else
         @articles = Article.all.select{|art| art.category_id.to_i == @category.id}
         if @articles == []
           @t = true
         else
-          
+
         end
       end
-  
+
     end
 
     def show
       @related_articles1 = []
       @article = Article.find(params[:id])
       @comment = Comment.new
-      @article_title_words = @article.title.downcase.split(' ') 
-      
+      @article_title_words = @article.title.downcase.split(' ')
+
       @related_articles = Article.all.select{|art| (art.title.downcase.split(' ') & @article_title_words).any? }
       @related_articles.delete_if{ |ra| ra == @article}
       # byebug
@@ -45,13 +47,13 @@ class ArticlesController < ApplicationController
       end
       # byebug
     end
-  
+
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_article
         @article = Article.find(params[:id])
       end
-  
+
       # Never trust parameters from the scary internet, only allow the white list through.
       def article_params
         params.fetch(:article, {})
